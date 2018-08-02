c737de7c11d7a527ab4edf93a097b55b86879393
Victor Ma <vic.ma@mail.utoronto.ca>
2018-08-02 17:51:43 -0400

    Create Term class

---
+++ machineepsilon/Term.java
@@ -0,0 +1,13 @@
+package machineepsilon;
+
+public class Term
+{
+    Fraction coefficient;
+    int exponent;
+
+    public Term(Fraction coefficient, int exponent)
+    {
+        this.coefficient = coefficient;
+        this.exponent = exponent;
+    }
+}
