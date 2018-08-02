b1bd96a04946bd234746cad5de86810aab303eee
Victor Ma <vic.ma@mail.utoronto.ca>
2018-08-02 18:00:39 -0400

    Add arithmetic methods to Term

--- machineepsilon/Term.java
+++ machineepsilon/Term.java
@@ -10,4 +10,35 @@ public class Term
         this.coefficient = coefficient;
         this.exponent = exponent;
     }
+
+    public Term(int coefficient, int exponent)
+    {
+        Term(new Fraction(coefficient), exponent);
+    }
+
+    public static Term add(Term t1, Term t2)
+    {
+        Fraction coefficient = Fraction.add(t1.getCoefficient(), t2.getCoefficient());
+        return new Term(coefficient, t1.getExponent());
+    }
+
+    public static Term subtract(Term t1, Term t2)
+    {
+        Fraction coefficient = Fraction.subtract(t1.getCoefficient(), t2.getCoefficient());
+        return new Term(coefficient, t1.getExponent());
+    }
+
+    public static Term multiply(Term t1, Term t2)
+    {
+        Fraction coefficient = Fraction.multiply(t1.getCoefficient(), t2.getCoefficient());
+        int exponent = t1.getExponent() + t2.getExponent();
+        return new Term(coefficient, exponent);
+    }
+
+    public static Term divide(Term t1, Term t2)
+    {
+        Fraction coefficient = Fraction.divide(t1.getCoefficient(), t2.getCoefficient());
+        int exponent = t1.getExponent() - t2.getExponent();
+        return new Term(coefficient, exponent);
+    }
 }
