6277fdae885a633398a9ef57d20ed8dc79361295
Victor Ma <vic.ma@mail.utoronto.ca>
2018-08-05 22:44:53 -0400

    Add generateProof method

--- machineepsilon/MachineEpsilon.java
+++ machineepsilon/MachineEpsilon.java
@@ -7,5 +7,13 @@ public class MachineEpsilon
         Polnomial function = limit.getFunction();
         int c = limit.getC();
         int l = limit.getL();
+
+        Polynomial difference = new Polynomial(function);
+        difference.subtractTerm(new Term(l, 0));  // |f(x)-L|
+        Polynomial quotient = Polynomial.divide(function, difference);
+        Inequality differenceXC = new Inequality(-1, difference, 1);  // Let |x-c| < 1
+        differenceXC.addTerm(c);   // -1 < x-c < 1  =>  c-1 < x < c+1
+        int m = Math.max(differenceXC.getLeft(), differenceXC.getRight()); // m = max{c-1, c+1}
+        // |x| < m
     }
 }
