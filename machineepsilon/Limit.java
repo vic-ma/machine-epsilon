a7856c0ba40c0a2c1c9007f2c70dfe5a36629813
Victor Ma <vic.ma@mail.utoronto.ca>
2018-08-24 22:51:26 -0400

    Add String constructor to Limit

--- machineepsilon/Limit.java
+++ machineepsilon/Limit.java
@@ -2,29 +2,52 @@ package machineepsilon;
 
 public class Limit
 {
-    private Polynomial function;
-    private int c;
-    private int l;
+    private Fraction c;
+    private Polynomial f;
+    private Fraction l;
 
-    public Limit(Polynomial function, int c, int l)
+    public Limit(Fraction c, Polynomial f, Fraction l)
     {
-        this.function = function;
         this.c = c;
+        this.f = f;
         this.l = l;
     }
 
-    public Polynomial getFunction()
+    public Limit(String limit)
     {
-        return function;
+        String[] splitLimit = limit.split(" ");
+        for (int i = 0; i < splitLimit.length; i++)
+        {
+            if (splitLimit[i].indexOf("→") == 1)
+            {
+                this.c = new Fraction(splitLimit[i++].substring(2));
+                this.f = new Polynomial(splitLimit[i++]);
+                this.l = new Fraction(splitLimit[i+1]);
+                return;
+            }
+        }
     }
 
-    public int getC()
+    public Fraction getC()
     {
         return c;
     }
 
-    public int getL()
+    public Polynomial getF()
+    {
+        return f;
+    }
+
+    public Fraction getL()
     {
         return l;
+    }
+
+    public static void main(String args[])
+    {
+        Limit l = new Limit("lim x→3 x^2-3x+1 = 1");
+        System.out.println(l.getC());
+        System.out.println(l.getF());
+        System.out.println(l.getL());
     }
 }
